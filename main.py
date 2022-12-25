c5a8de2d7dd429014673c5826187120f6fb4243a
Omid Jahandar <omid.jahandar.98@gmail.com>
2022-12-25 15:29:19 +0330

    initial

---
+++ main.py
@@ -0,0 +1,60 @@
+import xlsxwriter
+from tqdm import tqdm
+
+from test import take_data_from_db
+
+products = take_data_from_db()
+last_products = []
+
+# skuid, skuid_hb, name, id, market, price, brand, last_midified_date
+#
+
+for each in tqdm(products):
+    product = dict()
+    product['doc'] = each
+    # if each['sku_id'] is None:
+    #     product['sku_id'] = ''
+    # else:
+
+    # print(each['sku_id'])
+
+    product['sku_id'] = each['sku_id']
+    product['skuid_hb'] = each['skuid_hb']
+    product['name'] = each['name']
+    product['id'] = each['id']
+    product['market'] = each['market']
+    product['price'] = each['price']
+    product['brand'] = each['brand']
+    product['modified_date'] = each['modified_date']
+
+    last_products.append(product)
+
+workbook = xlsxwriter.Workbook('Example3.xlsx')
+worksheet = workbook.add_worksheet("My sheet")
+row = 0
+col = 0
+worksheet.write(row, col, 'sku_id')
+worksheet.write(row, col + 1, 'skuid_hb')
+worksheet.write(row, col + 2, 'name')
+worksheet.write(row, col + 3, 'id')
+worksheet.write(row, col + 4, 'market')
+worksheet.write(row, col + 5, 'price')
+worksheet.write(row, col + 6, 'brand')
+worksheet.write(row, col + 7, 'modified_date')
+worksheet.write(row, col + 8, 'doc')
+
+row = 1
+for each_product in tqdm(last_products):
+    col = 0
+    # print(each_product['sku_id'])
+    worksheet.write(row, col, str(each_product['sku_id']))
+    worksheet.write(row, col + 1, str(each_product['skuid_hb']))
+    worksheet.write(row, col + 2, str(each_product['name']))
+    worksheet.write(row, col + 3, str(each_product['id']))
+    worksheet.write(row, col + 4, str(each_product['market']))
+    worksheet.write(row, col + 5, str(each_product['price']))
+    worksheet.write(row, col + 6, str(each_product['brand']))
+    worksheet.write(row, col + 7, str(each_product['modified_date']))
+    worksheet.write(row, col + 8, str(each_product['doc']))
+    row += 1
+workbook.close()
---
+++ test.py
@@ -0,0 +1,40 @@
+import json
+import psycopg2
+import requests
+from tqdm import tqdm
+
+conn_new = psycopg2.connect(database="narmoni", user='postgres', password='4rf#dfer53RF4#@',
+                            host='54.90.147.228', port='5432')
+
+cursor = conn_new.cursor()
+HERO_API_ADDRESS = 'http://3.83.150.125:5000//data/dynamic/'
+
+
+def get_data_from_api(data):
+    market = data[1]
+    stock_id = f"/{data[0]}"
+    product_address = HERO_API_ADDRESS + market + stock_id
+    print(product_address)
+    try:
+        res = json.loads(requests.get(product_address, timeout=5).content)
+        return res
+    except Exception as e:
+        print(e)
+        pass
+
+
+def take_data_from_db():
+    dynamic_price_data = []
+    query_sting = f''' 
+  select stock_id , market  from dynamic.product
+  '''
+    cursor.execute(query_sting)
+    cursors = cursor.fetchall()
+
+    for index, each in enumerate(tqdm(cursors)):
+        try:
+            product = get_data_from_api(each)
+            dynamic_price_data.append(product)
+        except:
+            pass
+    return dynamic_price_data
